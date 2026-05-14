f106f1d49e9683f4ec97d2b50362dcf107f40289
Elisee dev <eliseeonyembo@gmail.com>
2026-05-14 20:07:41 +0200

    logo

--- src/Components/child/Logo.tsx
+++ src/Components/child/Logo.tsx
@@ -8,7 +8,7 @@ type Props ={
 export default function Logo({style,stspan}:Props) {
   return (
       <h1 className={style}>
-        <span className={stspan}><img src="/public/iconYk.png" className="w-8 h-8" alt="" />KIS</span> store
+        <span className={stspan}><img src="/iconYk.png" className="w-8 h-8" alt="" />KIS</span> store
       </h1>
   );
 }
